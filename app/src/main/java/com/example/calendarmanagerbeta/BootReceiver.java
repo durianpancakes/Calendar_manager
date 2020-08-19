61f1f30fb721365cc452b0a92c2539feb5fd4709
chanx <chanxuhui@outlook.com>
2020-08-20 00:31:05 +0800

    Added a few notifications for debug

--- app/src/main/java/com/example/calendarmanagerbeta/BootReceiver.java
+++ app/src/main/java/com/example/calendarmanagerbeta/BootReceiver.java
@@ -1,9 +1,16 @@
 package com.example.calendarmanagerbeta;
 
+import android.app.Notification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import static com.example.calendarmanagerbeta.MainActivity.CHANNEL_ID;
 
 public class BootReceiver extends BroadcastReceiver {
     private Context mContext;
@@ -12,8 +19,17 @@ public class BootReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         mContext = context;
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-            ReminderHelper reminderHelper = ReminderHelper.getInstance(mContext);
+            ReminderHelper reminderHelper = ReminderHelper.getInstance(context);
             reminderHelper.refreshDailyAlarms();
+            NotificationManagerCompat mNotificationManager;
+            Notification notification2 = new NotificationCompat.Builder(context, CHANNEL_ID)
+                    .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
+                    .setSmallIcon(R.drawable.ic_baseline_access_alarm_24)
+                    .setContentTitle("Boot completed")
+                    .setAutoCancel(true)
+                    .setOnlyAlertOnce(true).build();
+            mNotificationManager = NotificationManagerCompat.from(context);
+            mNotificationManager.notify(666, notification2);
         }
     }
 }
